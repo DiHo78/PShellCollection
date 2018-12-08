4e180838f1e190c7ce595c2115dd043ce1e4b7a6
Dirk Hondong <dirk@hondong.de>
2018-12-08 12:30:07 +0100

    Update SMOTest.ps1

--- BlogCode/SMOTest.ps1
+++ BlogCode/SMOTest.ps1
@@ -1,3 +1,3 @@
 add-type -Path 'D:\Program Files\Microsoft SQL Server\140\SDK\Assemblies\Microsoft.SqlServer.Smo.dll'
-$server = New-Object -TypeName Microsoft.SQLServer.Management.Smo.Server "SQL-MON-01"
+$server = New-Object -TypeName Microsoft.SQLServer.Management.Smo.Server "MeinTestServer"
 $server.Databases.Item("AdminDB")
